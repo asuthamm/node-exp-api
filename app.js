4f4e99b20c6d36613f29575af4eb7840d57d8574
asuthamm <asuthamm@gmail.com>
2020-05-19 23:30:39 -0400

    create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to ALEX Nodemon API!");
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
