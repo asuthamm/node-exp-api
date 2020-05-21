f7a1bda6dfbedd37499bd02cdf6c5707199d363d
asuthamm <asuthamm@gmail.com>
2020-05-21 13:37:01 -0400

    removed eslint

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
 const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
-const db = mongoose.connect("mongodb://localhost/bookAPI");
-const bookRouter = express.Router();
-const port = process.env.PORT || 3000;
-const Book = require("./models/bookModel");
 
-bookRouter.route("/books").get((req, res) => {
-  const query = {};
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(books);
-  });
-});
-bookRouter.route("/books/:bookId").get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(book);
-  });
-});
-app.use("/api", bookRouter);
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my Nodemon API!");
+  res.send("Welcome to ALEX Nodemon API***");
 });
 
 app.listen(port, () => {
