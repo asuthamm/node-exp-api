f0a0a3dce13408498764dabae9cb76376470847a
asuthamm <asuthamm@gmail.com>
2020-05-20 23:21:32 -0400

    add bookRouter

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
+const db = mongoose.connect("mongodb://localhost/bookAPI");
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require("./models/bookModel");
+
+bookRouter.route("/books").get((req, res) => {
+  const query = {};
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+  Book.find(query, (err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
+});
+bookRouter.route("/books/:bookId").get((req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(book);
+  });
+});
+app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to ALEX Nodemon API!");
+  res.send("Welcome to my Nodemon API!");
 });
 
 app.listen(port, () => {
