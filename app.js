3115041783d91c6701c14c7b3e1f8fee4c3bf780
asuthamm <asuthamm@gmail.com>
2020-05-21 17:21:51 -0400

    add mongoose

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
-const app = express();
-const bookRouter = express.Router();
+const mongoose = require("mongoose");
 
+const app = express();
+const db = mongoose.connect("mongodb://localhost/bookAPI");
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require("./models/bookModel");
 
 bookRouter.route("/books").get((req, res) => {
-  const response = { hello: "This is my API" };
-  res.json(response);
+  // const response = { hello: "This is my API" };
+  // res.json(response);
+  // .get((req, res) => {
+  const query = {};
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+  Book.find(query, (err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
 });
 app.use("/api", bookRouter);
 
