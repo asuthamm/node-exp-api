a2f892cf1c03e3b7cc086043d35fdf745a1d3128
asuthamm <asuthamm@gmail.com>
2020-07-18 21:57:10 -0400

    create booksController

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI');
+const db = mongoose.connect("mongodb://localhost/bookAPI");
 const port = process.env.PORT || 3000;
-const Book = require('./models/bookModel');
-const bookRouter = require('./routes/bookRouter')(Book);
+const Book = require("./models/bookModel");
+const bookRouter = require("./routes/bookRouter")(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api', bookRouter);
+app.use("/api", bookRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my Nodemon API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to my Nodemon API!");
 });
 
 app.listen(port, () => {
---
+++ controllers/booksController.js
@@ -0,0 +1,17 @@
+function booksController(Book) {
+  function get(req, res) {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  }
+  return { post, get };
+}
+
+module.exports = booksController;
